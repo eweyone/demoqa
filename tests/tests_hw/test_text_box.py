ad8204a89aa4074f72ca64a9a76475f94ea4f9e4
eweyone <piatashi69@gmail.com>
2025-09-24 23:14:12 +0300

    Fix 10_hw
    - Add variables for test values

--- tests/tests_hw/test_text_box.py
+++ tests/tests_hw/test_text_box.py
@@ -6,14 +6,17 @@ import time
 def test_text_box(browser):
     textbox_page = TextBox(browser)
 
+    full_name = 'test'
+    current_address = 'test-test'
+
     textbox_page.visit()
-    textbox_page.full_name_field.send_keys('test')
-    textbox_page.current_address_field.send_keys('test-test')
+    textbox_page.full_name_field.send_keys(full_name)
+    textbox_page.current_address_field.send_keys(current_address)
     textbox_page.submit_button.click()
     time.sleep(2)
     assert textbox_page.output_full_name.find_element()
     assert textbox_page.output_current_address.find_element()
-    assert textbox_page.output_full_name.get_text() == 'Name:test'
+    assert textbox_page.output_full_name.get_text() == f'Name:{full_name}'
 
     # Ошибка в html - лишний пробел перед двоеточием "Current Address :"
-    assert textbox_page.output_current_address.get_text() == 'Current Address :test-test'
+    assert textbox_page.output_current_address.get_text() == f'Current Address :{current_address}'
